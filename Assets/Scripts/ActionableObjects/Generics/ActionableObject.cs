2c5a3d05d9d4c51d34b65e8d395d6f6e72ec0c7b
Ale Sanchez <alesanchezmedina@gmail.com>
2021-05-11 19:19:55 +0200

    Added generics scripts

---
+++ Assets/Scripts/ActionableObjects/Generics/ActionableObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ActionableObject : MonoBehaviour, IActionableObject
+{
+
+    [Header("UI")]
+    [Tooltip("The text that will appear in the overlay before interacting with this object")]
+    [SerializeField] string interactionText = "open";
+
+    [Tooltip("The text that will appear in the overlay after interacting with this object")]
+    [SerializeField] string interactionTextReverse = "close";
+
+    protected bool m_interacted = false;
+
+    protected bool m_interactive = true;
+
+    private void Start() {
+        InnerStart();
+    }
+
+    protected abstract void InnerStart();
+
+    public string GetInteractionText()
+    {
+        return !m_interacted ? interactionText : interactionTextReverse;
+    }
+
+    public void Interact() {
+        InnerInteract();
+        m_interacted = !m_interacted;
+    }
+
+    protected virtual void InnerInteract() {}
+
+    public virtual bool IsInteracterActive() => m_interactive;
+
+    public virtual void SetIsInteractive(bool isInteractive) => m_interactive = isInteractive;
+}
---
+++ Assets/Scripts/ActionableObjects/Generics/InteractionText.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class InteractionText : MonoBehaviour
+{
+    private Text m_text;
+    
+    private string m_initialText;
+    private string m_interactionText;
+
+    private void Start() {
+        m_text = GetComponent<Text>();
+
+        m_initialText = m_text.text;
+    }
+
+    private void Update() {
+        if(m_text.enabled) {
+            m_text.text = $"{m_initialText}{m_interactionText}";
+        }
+    }
+
+    public void SetInteractionText(string interactionText) {
+        m_interactionText = interactionText;
+    }
+
+    public void Enable() {
+        m_text.enabled = true;
+    }
+
+    public void Disable() {
+        m_text.enabled = false;
+    }
+
+    public bool IsEnabled() => m_text.enabled;
+}
